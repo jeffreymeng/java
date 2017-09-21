98b6a4fb1aa972c5cf1f2a6e8e6ba4fbe2b0e883
java student 1 <javastudent1@kms-cart11-16.cupertino.k12.ca.us>
2017-09-21 08:50:10 -0700

    files

---
+++ Ch3Conditionals/src/ifElseStatements/OrderTwoInts.java
@@ -0,0 +1,32 @@
+/*
+ * Programmer: Jeffrey Meng
+ * Date: Sep 21, 2017
+ * Purpose: Ask the user to enter two numbers. Print out in ascending order.
+ */
+
+package ifElseStatements;
+
+import java.util.Scanner;
+
+public class OrderTwoInts {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int num1, num2;
+		System.out.println("Enter a number:");
+		num1 = in.nextInt();
+		System.out.println("Enter another number:");
+		num2 = in.nextInt();
+		
+		if (num1 == num2) {
+			System.out.println("The numbers are equal!");
+		} else if (num1 > num2) {
+			System.out.println(num1 + ", " + num2);
+		} else {
+			System.out.println(num2 + ", " + num1);
+		}
+		
+	}
+
+}
---
+++ Ch3Conditionals/src/ifElseStatements/TelegramCost.java
@@ -0,0 +1,25 @@
+/*
+ * Programmer: Jeffrey Meng
+ * Date: Sep 21, 2017
+ * Purpose: Determine the cost of a telegram. $8.50 for first 15 words, 0.25 each over.
+ */
+
+package ifElseStatements;
+
+import java.util.Scanner;
+
+public class TelegramCost {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number of words in your telegram:");
+		int words = in.nextInt();
+		
+		if (words > 15) {
+			System.out.printf("Your " + words + " word telegram will cost $%.2f.", 8.5 + ((words - 15) * 0.25));
+		} else {
+			System.out.println("Your " + words + " word telegram will cost $8.50.");
+		}
+		
+		in.close();
+	}
+}
