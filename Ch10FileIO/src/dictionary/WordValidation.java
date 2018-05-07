b9736f696e2c87a747ff16d59bccf4a3ab2b644e
Jeffrey Meng <jeffkmeng@gmail.com>
2018-05-07 09:18:14 -0700

    sync code

--- Ch10FileIO/src/dictionary/WordValidation.java
+++ Ch10FileIO/src/dictionary/WordValidation.java
@@ -1,13 +1,35 @@
 package dictionary;
 
-import java.io.file;
+import java.io.File;
 import java.util.Scanner;
 
 class WordValidation {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String input;
+		boolean validateAgain = true;
+
+		validate();
+		
+		while(validateAgain) {
+			System.out.print("Validate another word? (Y)es/(n)o: ");
+			input = in.nextLine();
+			if (input.equalsIgnoreCase("y")) {
+				validate();	
+			} else {
+				validateAgain = false;
+				break;
+			}
+			
+		}		
+		
+
+	}
+
+	public static void validate() {
 		Scanner in, reader;
-		String filepath = "src/words.txt";
+		String filepath = "words.txt";
 		File file;
 		in = new Scanner(System.in);
 		System.out.println("Enter a word:");
@@ -15,8 +37,8 @@ class WordValidation {
 		try {
 			file = new File(filepath);
 			reader = new Scanner(file);
-			while (input.hasNextLine()) {
-				String line = input.nextLine();
+			while (reader.hasNextLine()) {
+				String line = reader.nextLine();
 				if (line.equals(input)) {
 					System.out.println("That's a word!");
 					in.close();
