80a1888a2a42fafdaf3360421ed7863ac17b70c6
Jeffrey Meng <jeffkmeng@gmail.com>
2018-05-22 09:18:03 -0700

    sync code

--- javaclass/Ch10FileIO/src/SodukuBoardValidator.java
+++ javaclass/Ch10FileIO/src/SodukuBoardValidator.java
@@ -4,13 +4,20 @@
  * Purpose: Validate a text file containing a soduku board
  */
 
-class SodukuBoardValidator.java {
+import java.util.Scanner;
+import java.io.File;
+
+class SodukuBoardValidator {
 	
 	int[][] board;
 
 
-	public static void main(Strign[] args) {
-
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the path of the file to read from: ");
+		String path = in.nextLine();
+		SodukuBoardValidator validator = new SodukuBoardValidator(path);
+		System.out.println(validator.validate());
 
 
 	}
@@ -45,6 +52,9 @@ class SodukuBoardValidator.java {
 			
                         for (int i = 0; reader.hasNextLine(); i ++) {
                                 line = reader.nextLine().split(",");
+				if (line[0] == null) {
+					continue;
+				}
 				for (int j = 0; j < board[i].length; j ++) {
 					board[i][j] = Integer.parseInt(line[j]);
 
@@ -67,36 +77,55 @@ class SodukuBoardValidator.java {
 	public boolean validate() {
 
 		boolean[] numList = new boolean[9];//boolean[] init to false - this numList is for rows, it can be reset after each iteration
-		boolean[][] colNumList = new boolean[9][9];//this numList is for each column, the x is the column, y is numbers for each column. - we need a 2d array because column
-		//is not self contained; it's data cannot reset between iterations.
 		String error = "";//TODO: consider printing error
-
+		
 		for (int i = 0; i < board[0].length; i ++) {
 			//check the rows - i is the row, col is the column
 			numList = new boolean[9];//reset numList to all false
 			for (int col = 0; col < board[0].length; col ++) {
 				if (numList[board[i][col] - 1]) {
-					error = "Duplicate number '" + board[i][col] + "' in row at location " + (i + 1) + "," + (col + 1) + ".";
+					error = "Duplicate number '" + board[i][col] + "' in row at location " + (i + 1) + ", " + (col + 1) + ".";
 					return false;
 				}
 				numList[board[i][col] - 1] = true;//set the number to used. (start counting array from 0, so subtract one)
 
 			}
-			
+		}
+
+		for (int i = 0; i < board.length; i ++) {	
 			//check the columns - now i is the column
-			numList = new boolean[9];//reset numList to all false
+			numList = new boolean[9];
 			for (int row = 0; row < board.length; row ++) {
-				if (colNumList[i][board[row][i]]) {
-					error = "Duplicate number '" + board[row][i] + "' in column at location " + (row + 1) + "," + (i + 1) + ".";
+				if (numList[board[row][i] - 1]) {
+					error = "Duplicate number '" + board[row][i] + "' in column at location " + (row + 1) + ", " + (i + 1) + ".";
                                         return false;
+				} else {
+					numList[board[row][i] - 1] = true;
 				}
 				
 
 			}
-
-			
-		
+	
 		}
+		
+		boolean[][][]blockNumList = new boolean[3][3][9];//numList for each x, y block, where top left is 1, 1, and bottom right is 3, 3. Middle is 2, 2.
+		for (int x = 0; x < 9; x ++) {
+			for (int y = 0; y < 9; y ++) {
+				//check each 3 by 3 'block'
+				int blockx = (int) Math.ceil((double) (x + 1) / 3) - 1;//convert coordinates starting from 1 to coordinates starting from 0
+				int blocky = (int) Math.ceil((double) (y + 1) / 3) - 1;				
+				if (blockNumList[blockx][blocky][board[y][x] - 1]) {// In a 2d array, x,y is inverted because it is row first, then column.	
+					error = "Duplicate number '" + board[y][x] + "' in block at location " + (x + 1) + ", " + (y + 1) + ".";
+					return false;
+				} else {
+
+					blockNumList[blockx][blocky][board[y][x] - 1] = true;
+				}		
+			}
+		}
+		//TODO: remove
+		System.out.println(error);
+		return true;
 
 	}	
 
